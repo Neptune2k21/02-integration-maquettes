c6c41dceb6f232f727d10ea0d89f73fb4c1e2d34
Neptune2k21 <mamadou_cisse01@etu.u-bourgogne.fr>
2025-10-11 16:07:44 +0200

    question 4.3 : metadata page catégorie

--- src/app/[categorySlug]/page.tsx
+++ src/app/[categorySlug]/page.tsx
@@ -1,13 +1,30 @@
 import { PRODUCTS_CATEGORY_DATA } from "@arthur.eudeline/starbucks-tp-kit/data";
 import { ProductGridLayout, ProductCardLayout, Button, BreadCrumbs, SectionContainer } from "@arthur.eudeline/starbucks-tp-kit";
 import { cache } from "react";
-
+import type { Metadata } from "next";
 /**
  * Récupère une catégorie produit à partir de son slug
  */
 const getCategory = cache(async (slug: string) => {
   return PRODUCTS_CATEGORY_DATA.find(cat => cat.slug === slug) ?? null;
 });
+
+export async function generateMetadata({ params }: { params: Promise<{ categorySlug: string }> }): Promise<Metadata> {
+  const { categorySlug } = await params;
+  const category = PRODUCTS_CATEGORY_DATA.find(cat => cat.slug === categorySlug);
+  
+  if (!category) {
+    return {
+      title: "Catégorie introuvable",
+      description: "Cette catégorie n'existe pas.",
+    };
+  }
+
+  return {
+    title: category.name,
+    description: `Trouvez votre inspiration avec un vaste choix de boissons Starbucks parmi nos produits ${category.name}`,
+  };
+}
 
 export default async function CategoryPage({ params }: { params: Promise<{ categorySlug: string }> }) {
   const { categorySlug } = await params;
