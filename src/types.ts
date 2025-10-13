c0b8e7c485ebada8b789e7f5cd9fbfe390671c65
Neptune2k21 <mamadou_cisse01@etu.u-bourgogne.fr>
2025-10-13 11:38:56 +0200

    question 2.1 : création types panier

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,19 @@ export type ProductFiltersResult = {
     categoriesSlug: string[];
     search?: string;
 }
+
+
+export type ProductData = {
+id: string | number;
+name: string;
+price: number;
+};
+
+export type ProductLineData = {
+product: ProductData;
+qty: number;
+};
+
+export type CartData = {
+lines: ProductLineData[];
+};
